2d3677e311da4b40faa7d06d165bb633c6b95ff9
yanxin <tmlx1990@163.com>
2016-03-08 17:44:09 +0800

    提交模拟生成代码

---
+++ src/main/java/org/mybatis/generator/test/StartUp.java
@@ -0,0 +1,29 @@
+package org.mybatis.generator.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+
+public class StartUp {
+	public static void main(String[] args) throws Exception {
+		List<String> warnings = new ArrayList<String>();
+    	File configFile = new File(StartUp.class.getResource("/generatorConfig.xml").toURI());
+    	ConfigurationParser cp = new ConfigurationParser(warnings);
+    	Configuration config = cp.parseConfiguration(configFile);
+    	DefaultShellCallback shellCallback = new DefaultShellCallback(true);
+    	MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, shellCallback, warnings);
+    	myBatisGenerator.generate(null);
+    	System.out.println(warnings);
+
+	}
+}
