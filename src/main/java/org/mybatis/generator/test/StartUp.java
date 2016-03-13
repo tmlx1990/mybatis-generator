88dce4b5b3279f0db2008886ee989c4a8f9addd4
yanxin <tmlx1990@163.com>
2016-03-14 00:34:17 +0800

    读取pdm测试

--- src/main/java/org/mybatis/generator/test/StartUp.java
+++ src/main/java/org/mybatis/generator/test/StartUp.java
@@ -2,12 +2,17 @@ package org.mybatis.generator.test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.Namespace;
+import org.jdom.input.SAXBuilder;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
@@ -16,14 +21,29 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class StartUp {
 	public static void main(String[] args) throws Exception {
-		List<String> warnings = new ArrayList<String>();
+		/*List<String> warnings = new ArrayList<String>();
     	File configFile = new File(StartUp.class.getResource("/generatorConfig.xml").toURI());
     	ConfigurationParser cp = new ConfigurationParser(warnings);
     	Configuration config = cp.parseConfiguration(configFile);
     	DefaultShellCallback shellCallback = new DefaultShellCallback(true);
     	MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, shellCallback, warnings);
     	myBatisGenerator.generate(null);
-    	System.out.println(warnings);
-
+    	System.out.println(warnings);*/
+		
+		//读取指定PDM
+		File file = new File(StartUp.class.getResource("/1.pdm").toURI());
+		//FileReader fileReader = new FileReader(file);
+		Document document = new SAXBuilder().build(file);
+		Element root = document.getRootElement();
+		Namespace na = root.getNamespace("a");
+		Namespace nc = root.getNamespace("c");
+		Namespace no = root.getNamespace("o");
+		
+		Element dbName = document.getRootElement().getChild("RootObject",no)
+				.getChild("Children",nc)
+				.getChild("Model",no)
+				.getChild("Name",na);
+		String DBName = dbName.getText();
+		System.out.println("1");
 	}
 }
